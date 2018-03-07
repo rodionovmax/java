319ee1589d25bb2999fdce04c5e11dd13fa91b02
rodionovmax90test@gmail.com <rodionovmax90test@gmail.com>
2018-03-07 17:23:38 -0500

    project untied from repository

--- sandbox/src/main/java/ru/stqa/pft/sandbox/helloworld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/helloworld.java
@@ -2,7 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class helloworld {
 
-	public static void main(String[] args) {
-System.out.println("Hello world!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 
+}
