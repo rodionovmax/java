fefe381e23a54b76ae9a2caae6180b22dd4e8acf
rodionovmax90test@gmail.com <rodionovmax90test@gmail.com>
2018-03-07 18:47:58 -0500

    a few function added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/helloworld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/helloworld.java
@@ -3,7 +3,28 @@ package ru.stqa.pft.sandbox;
 public class helloworld {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   hello("world");
+   hello("Max");
+
+   double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
+
+    double x = 6;
+    double y = 9;
+    System.out.println("Площадь прямоугольника со сторонами " + x + " и " + y + " = " + area(x,y));
   }
 
+
+  public static void hello(String smt){
+//    String smt = "world";
+    System.out.println("Hello " + smt);
+  }
+
+  public static double area(double len){
+    return len*len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
 }
